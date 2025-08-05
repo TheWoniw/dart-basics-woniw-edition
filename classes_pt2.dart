63cc7425d858ab664b9da45013fe6c517be35aea
woniw <notfoolish626@gmail.com>
2025-08-05 10:29:22 -0600

    updated classes_pt2.dart

--- classes_pt2.dart
+++ classes_pt2.dart
@@ -4,6 +4,14 @@ class Product {
   int stock;
 
   Product(this.name, this.genre, this.stock);
+
+  void check_stock() {
+    if (stock <= 0) {
+      print("${name} is out of stock");
+    } else {
+      print("${name} is still in stock, ${stock} is remaining");
+    }
+  }
 }
 
 void main() {
@@ -12,4 +20,6 @@ void main() {
   print("Phone brand: ${phone.name}");
   print("Phone genere ${phone.genre}");
   print("Phone stock: ${phone.stock}");
+
+  phone.check_stock();
 }
